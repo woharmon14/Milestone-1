7d53d9b8c6ea62e8eb18c463b6f430d55fbf9bf7
woharmon14 <woharmonscheiz@gmail.com>
2017-05-11 19:17:48 +0800

    milestone1
    blog

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    $("#owl-demo").owlCarousel({
+        
+        autoPlay: 2000, //Set AutoPlay to 3 seconds
+        items: 6,
+        itemsDesktop: [1199,3],
+        itemsDesktopSmall: [979,3]
+        
+        
+    });
+    
+});
